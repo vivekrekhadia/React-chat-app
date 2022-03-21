9050ec47dae7af2ebc26580ab65d3311c69c59f7
vivek <vivekrekhadia007@gmail.com>
2022-03-21 18:58:24 +0530

    file changes

--- src/Redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { chatSlice } from "./ChatSlice";
-
-const store = configureStore({
-  reducer: { Chat: chatSlice.reducer },
-});
-
-export default store;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import store from "./Redux/Store";
+
 import ActiveContext from "./Context/ActiveContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <ActiveContext>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </ActiveContext>
   </React.StrictMode>,
   document.getElementById("root")
